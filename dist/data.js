8833971e1b629777e0d61f2e13e570f5885a92f4
ryu1uga <20211953@aloe.ulima.edu.pe>
2025-06-03 12:22:01 -0500

    Backend setup done

---
+++ dist/data.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.gameLists = void 0;
+exports.gameLists = [
+    {
+        id: 1,
+        descripcion: "dormir"
+    },
+    {
+        id: 2,
+        descripcion: "comer"
+    }
+];
---
+++ src/data.ts
@@ -0,0 +1,15 @@
+export interface Game {
+    id : number
+    descripcion : string
+}
+
+export const gameLists : Game[] = [
+    {
+        id : 1,
+        descripcion : "dormir"
+    },
+    {
+        id : 2,
+        descripcion : "comer"
+    }
+]
